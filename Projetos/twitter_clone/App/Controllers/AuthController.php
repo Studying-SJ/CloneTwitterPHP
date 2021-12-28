5873a71397ea6cb592a7839ebd7f44d027919fda
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-12-28 01:47:56 -0300

    Edit (encrypt password )

--- Projetos/twitter_clone/App/Controllers/AuthController.php
+++ Projetos/twitter_clone/App/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Action {
 		$usuario = Container::getModel('Usuario');
 
 		$usuario->__set('email', $_POST['email']);
-		$usuario->__set('senha', $_POST['senha']);
+		$usuario->__set('senha', md5($_POST['senha']));
 
 		$usuario->autenticar();
 
--- Projetos/twitter_clone/App/Controllers/IndexController.php
+++ Projetos/twitter_clone/App/Controllers/IndexController.php
@@ -33,7 +33,7 @@ class IndexController extends Action {
 
 		$usuario->__set('nome', $_POST['nome']);
 		$usuario->__set('email', $_POST['email']);
-		$usuario->__set('senha', $_POST['senha']);
+		$usuario->__set('senha', md5($_POST['senha']));
 
 		
 		if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) {
